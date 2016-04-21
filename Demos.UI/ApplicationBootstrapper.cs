d2d6a69dc7b688853f4cda7c822c80e14b28ac7c
Balwinder Katoch <vickykatoch@yahoo.com>
2016-04-21 16:37:01 -0400

    Initial Commit

---
+++ Demos.UI/ApplicationBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Demos.UI.Views;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using System.Windows;
+
+namespace Demos.UI
+{
+    class ApplicationBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<ApplicationShell>();
+        }
+        protected override void InitializeShell()
+        {
+            App.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# avam-exp
